4a7a8191b3ea8d513866e0a8873bc792157d1097
Oleksii304 <oleksii.rybak@global.com>
2021-10-29 01:23:07 +0100

    adding multiplication

--- src/main/java/com/oleksii/calculator/Calculator.java
+++ src/main/java/com/oleksii/calculator/Calculator.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
 
+    int multiplication(final int a, final int b) {
+        return a * b;
+    }
+
     int sum(final int a, final int b) {
         int result = a + b;
         return result;
