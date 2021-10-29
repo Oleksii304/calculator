17e2626954a8326ae3c98741613a54d3f1bc01fc
Oleksii304 <oleksii.rybak@global.com>
2021-10-30 00:29:14 +0100

    change

--- src/main/java/com/oleksii/calculator/Calculator.java
+++ src/main/java/com/oleksii/calculator/Calculator.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
 
+    int divide(final int a, final int b) {
+        return a / b;
+    }
+
     int multiplication(final int a, final int b) {
         return a * b;
     }
 
     int sum(final int a, final int b) {
-        int result = a + b;
+        final int result = a + b;
         return result;
     }
 
